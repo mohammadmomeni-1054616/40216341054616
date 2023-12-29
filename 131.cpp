d83850c18a1e77884730769ebc820dc03cd76a46
mohammadmomeni-1054616 <155058306+mohammadmomeni-1054616@users.noreply.github.com>
2023-12-29 18:09:38 +0330

    Add files via upload

---
+++ 131.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i,mahdi[100];
+
+    for(i=0;i<100;i++)
+        cin>>mahdi[i];
+
+    for(i=0;i<100;i+=3)
+        cout<<mahdi[i];
+}
